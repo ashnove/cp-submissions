8a52b6a08d184a3338134f3722abe3d41b3ab81f
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2020-10-12 07:16:00 +0530

    Add solution for problem `B - Battle` Link: https://atcoder.jp/contests/abc164/tasks/abc164_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc164/submissions/17359103

---
+++ atcoder/abc164/B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define Knucklehead ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m)
+     {return vector< vector<ll> >(n, vector<ll>(m));}
+
+bool compare(char &s1,char &s2){return s1 > s2;}
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b)
+{
+    if(a.first < b.first )
+        return true;
+    if( a.first==b.first && a.S != b.second )
+       return true;
+    return false;
+}
+//me
+ll const mod = 998244353;
+ll const inf = 1e18;
+ll const maxn = 1e6+1;
+
+void solve()
+{    
+     ll a,b,c,d; cin >> a >> b >> c >> d;
+
+     ll turn = 1;
+     while( a>0 && c>0 ){
+          if(turn)
+               c-=b,turn=0;
+          else
+               a-=d,turn=1;
+          
+     }
+     if(c<=0)
+          cout << "Yes";
+     else
+          cout << "No";
+     
+}
+
+int main() {
+Knucklehead
+
+          ll t;
+          /**/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
