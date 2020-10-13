5efe575b15eb99710f2ecaf4eb50c51435f04516
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2020-10-14 03:22:00 +0530

    Add solution for problem `A - The Number of Even Pairs` Link: https://atcoder.jp/contests/abc159/tasks/abc159_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc159/submissions/17386896

---
+++ atcoder/abc159/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define Knucklehead ios_base::sync_with_stdio(false);cin.tie(NULL);
+//#define mod 100000007
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b) 
+{ 
+    if(a.first < b.first )
+        return true;
+    if( a.first==b.first && a.S != b.second )
+       return true;
+    return false;
+}
+//me
+ll const mod = 998244353;
+ll const inf = 1e18;
+ll const maxn = 5e5+5;
+
+void solve()
+{     
+      ll n,m; cin >> n >> m;
+      cout << ( n*(n-1)/2 + m*(m-1)/2) ;     
+      
+}    
+    
+int main() {
+Knucklehead 
+//*
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+        
+          ll t;
+          /**/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+ 
+ 
+    return 0;
+}
