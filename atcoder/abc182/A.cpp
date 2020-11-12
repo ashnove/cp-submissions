6b0f48f4837067e0cc6a68b82728607a7936ed6f
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2020-11-12 14:41:00 +0530

    Add solution for problem `A - twiblr` Link: https://atcoder.jp/contests/abc182/tasks/abc182_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc182/submissions/18062039

---
+++ atcoder/abc182/A.cpp
@@ -0,0 +1,48 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define Knucklehead ios_base::sync_with_stdio(false);cin.tie(NULL);
+//#define mod 100000007                
+//me
+ll const mod = 1e9+7;
+ll const inf = 1e18;
+ll const maxn = 1e3+1;
+
+  
+void solve()      
+{     
+      ll a,b; cin >> a >> b;
+      ll tot = 2*a + 100;
+      cout << max( 0ll, tot - b ) << endl;     
+
+                                       
+} 
+int main() {                      
+Knucklehead 
+//*
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+        
+          ll t;
+          /**/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+ 
+ 
+    return 0;
+}           
