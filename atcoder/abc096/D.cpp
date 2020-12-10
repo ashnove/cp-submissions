5fd6313b9bdfe4050a0abd3de9cb4eda925def4f
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2020-12-11 04:03:00 +0530

    Add solution for problem `D - Five, Five Everywhere` Link: https://atcoder.jp/contests/abc096/tasks/abc096_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc096/submissions/18676848

---
+++ atcoder/abc096/D.cpp
@@ -0,0 +1,75 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define Knucklehead ios_base::sync_with_stdio(false);cin.tie(NULL);
+//#define mod 100000007
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b) 
+{ 
+    if(a.first < b.first )
+        return true;
+    if( a.first==b.first && a.S != b.second )
+       return true;
+    return false;
+}
+//me
+ll const mod = 1e9;
+ll const inf = 1e17;
+ll const maxn = 1e5+1;
+
+bool isprime(ll x){
+      for(ll i = 2; i*i<=x; i++)
+            if(x%i==0)
+                  return 0;
+      return 1;
+}
+void solve()
+{
+      
+      ll n; cin >> n;
+
+      ll ans[56];
+
+      ll it = 0;
+      for(ll i = 2; i<=55555 && it<56; i++){
+            if( i%10 == 1 && isprime(i) )
+                  ans[it++]=i;
+      }
+
+      for(ll i = 0; i<n; i++)
+            cout << ans[i] << " ";
+
+
+}
+                
+int main() {
+Knucklehead 
+//*
+#ifndef ONLINE_JUDGE    
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+        
+          ll t;
+          /**/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+ 
+ 
+    return 0;
+}     
