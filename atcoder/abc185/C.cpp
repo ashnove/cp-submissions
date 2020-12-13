c675e157b9a4b65fe2bad81f0b9b6291a07ed751
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2020-12-13 18:11:00 +0530

    Add solution for problem `C - Duodecim Ferra` Link: https://atcoder.jp/contests/abc185/tasks/abc185_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc185/submissions/18744957

---
+++ atcoder/abc185/C.cpp
@@ -0,0 +1,71 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define Knucklehead ios_base::sync_with_stdio(false);cin.tie(NULL);
+//#define mod 100000007
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b) 
+{ 
+    if(a.first < b.first )
+        return true;
+    if( a.first==b.first && a.S != b.second )
+       return true;
+    return false;
+}
+//me
+ll const mod = 1e9;
+ll const inf = 1e17;
+ll const maxn = 1e5+1;
+
+ll ncr(ll n, ll k)
+{
+      //if (k > n) return 0;
+      ll res = 1;
+
+      for(ll d = 1; d <= k; d++)
+      {
+            // cout << res << endl;
+            res *= n--;
+            res /= d;
+      }
+      return res;
+}
+
+void solve()
+{
+      ll n; cin >> n;
+      cout << ncr(n - 1, 11);      
+
+}
+                
+int main() {
+Knucklehead 
+//*
+#ifndef ONLINE_JUDGE    
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+        
+          ll t;
+          /**/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+ 
+ 
+    return 0;
+}     
