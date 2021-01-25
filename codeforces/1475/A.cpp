6c6bf2141ce7dc52fa772e42d408425ded717f51
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-01-25 20:32:00 +0530

    Add solution for problem `A - Odd Divisor` Link: https://codeforces.com/contest/1475/problem/A Tags: math, number theory, *900 Ref: https://codeforces.com/contest/1475/submission/105308473

---
+++ codeforces/1475/A.cpp
@@ -0,0 +1,58 @@
+/*@author : ashnove*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b) 
+{ 
+    if(a.first < b.first )
+        return true;
+    if( a.first==b.first && a.S != b.second )
+       return true;
+    return false;
+}
+//me19U10584
+ll const mod = 1e9+7;
+ll const inf = 1e18;
+ll const maxn = 5e5+1;
+
+void solve()
+{      
+      ll n; cin >> n;
+
+      while(n%2==0){
+            n/=2;
+      }
+      if(n>1 && n&1)cout << "YES\n";
+      else cout << "NO\n";
+}
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);cout.tie(NULL);
+//*
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/
+          ll t;
+          /*/t=1;
+          /*/cin>>t;/**/
+          f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+          }
+ 
+ 
+    return 0;
+}     
