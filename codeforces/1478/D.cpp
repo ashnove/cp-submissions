d1bb5c3c2e8a35a010886d5edd8062ed413e9d99
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-01-29 01:12:00 +0530

    Add solution for problem `D - Nezzar and Board` Link: https://codeforces.com/contest/1478/problem/D Tags: constructive algorithms, math, number theory, *1800 Ref: https://codeforces.com/contest/1478/submission/105783195

---
+++ codeforces/1478/D.cpp
@@ -0,0 +1,62 @@
+/*@author : ashnove*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b){ 
+    if(a.first < b.first ) return 1;
+    if( a.first==b.first && a.S != b.second ) return 1;
+    return 0;
+}              
+// ll const mod =  63018038201;4
+// 1125899906842597
+ll const mod = 1e9+7;
+ll const inf = 1e18;
+ll const maxn = 5e5+1;
+
+void solve()
+{
+      ll n; cin >> n;
+      ll k; cin >> k;
+      ll a[n];
+      for(ll i = 0; i < n; i++)
+            cin >> a[i];
+      sort(a,a+n);
+      ll g=0;
+      for(ll i = 1; i < n; i++)
+            g = __gcd(g, a[i]-a[i-1]);
+      bool ok = 1;
+      for(ll i = 0; i<n; i++){
+            if( abs(k-a[i])%g )ok=0;
+      }
+      cout << (!ok?"NO\n":"YES\n");
+}     
+
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);cout.tie(NULL);
+//*
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+            ll t;
+            /*/t=1;
+            /*/cin>>t;/**/
+            f(testcases,0,t){
+               //cout << "Case #" <<testcases+1<<": ";
+               solve();
+            } 
+      return 0;
+}      
