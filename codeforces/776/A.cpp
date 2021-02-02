a30da8e4ca25092e0022e4f6beb70d825afc1014
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-02-03 01:25:00 +0530

    Add solution for problem `A - A Serial Killer` Link: https://codeforces.com/contest/776/problem/A Tags: brute force, implementation, strings, *900 Ref: https://codeforces.com/contest/776/submission/106316363

---
+++ codeforces/776/A.cpp
@@ -0,0 +1,67 @@
+/*@author : ashnove*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m){return vector< vector<ll> >(n, vector<ll>(m));}
+bool compare(char &s1,char &s2){return s1 > s2;} 
+bool sortmahstyle(const pair<ll,ll> &a,  const pair<ll,ll> &b){ 
+    if(a.first < b.first ) return 1;
+    if( a.first==b.first && a.S != b.second ) return 1;
+    return 0;
+}  
+bool comp( const array<ll,3> &a, const array<ll,3> &b ){
+      if( a[0] < b[0] )
+            return 1;
+      if( a[0] == b[0] && a[1] > b[1] ) 
+            return 1;
+      return 0;
+}            
+// ll const mod =  63018038201;4
+// 1125899906842597
+ll const mod = 1e9+7;
+ll const inf = 1e15;
+ll const maxn = 1e6+1;
+
+void solve()
+{
+      
+      string a,b; cin >> a >> b;
+      cout << a << " " << b << endl;
+      ll n; cin >> n;
+      f(i,0,n){
+            string x,y; cin >> x >> y;
+            if( x==a )
+                  a = y;
+            else if(x==b)
+                  b = y;
+            cout << a << " " << b << endl;
+      }     
+      
+}     
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);cout.tie(NULL);
+//*
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+/**/ 
+            ll t;
+            /**/t=1;
+            /*/cin >> t;/**/
+            f(testcases,0,t){
+               // cout << "Case " <<testcases+1<<": ";
+               solve();
+            } 
+      return 0;
+}      
