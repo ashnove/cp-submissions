61ce087b21f864844f6023a7bf9d27848007620e
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-03-06 17:44:00 +0530

    Add solution for problem `C - Squared Error` Link: https://atcoder.jp/contests/abc194/tasks/abc194_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc194/submissions/20700172

---
+++ atcoder/abc194/C.cpp
@@ -0,0 +1,74 @@
+/*@author : ashnove*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define lld long double
+#define F first
+#define S second
+#define endl "\n"
+#define nl cout<<endl;
+#define pb push_back
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define mat vector<vector<ll>>
+mat cn(ll n, ll m) {return vector< vector<ll> >(n, vector<ll>(m));}
+bool comp1(char &s1, char &s2) {return s1 > s2;}
+bool comp2(const pair<ll, pair<ll, ll>> &a,  const pair<ll, pair<ll, ll>> &b) {
+      if (a.first > b.first ) return 1;
+      if ( a.first == b.first && a.S.S > b.S.S ) return 1;
+      return 0;
+}
+struct custom_hash {
+      static uint64_t splitmix64(uint64_t x) {
+            // http://xorshift.di.unimi.it/splitmix64.c
+            x += 0x9e3779b97f4a7c15;
+            x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+            x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+            return x ^ (x >> 31);
+      }
+      size_t operator()(uint64_t x) const {
+            static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+            return splitmix64(x + FIXED_RANDOM);
+      }
+};
+ll const mod = 1e9 + 7;
+ll const inf = 1e17;
+ll const maxn = 2e5 + 1;
+
+void solve()
+{
+      ll n; cin >> n;
+      ll a[n]; f(i,0,n) cin >> a[i];
+      ll sum = a[0];
+      ll sqsum = a[0]*a[0];
+      ll ans = 0;
+      for(ll i = 1; i < n; i++){
+
+            ans += i*a[i]*a[i] + sqsum - 2*a[i]*sum;
+            sum += a[i]; 
+            sqsum += a[i]*a[i];
+
+      }
+      cout << ans << endl;
+}
+
+
+int main() {
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+//*
+#ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+#endif
+      /**/
+      ll t;
+      /**/t = 1;
+      /*/cin >> t; /**/
+      f(testcases, 0, t) {
+            //cout << "Case #" <<testcases+1<<": ";
+            solve();
+      }
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
+      return 0;
+}
