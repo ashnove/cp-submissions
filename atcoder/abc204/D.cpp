3a5dd167ea1de6ea6ccacc365081c9fd16af3a01
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-06-07 11:47:00 +0530

    Add solution for problem `D - Cooking` Link: https://atcoder.jp/contests/abc204/tasks/abc204_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc204/submissions/23271036

---
+++ atcoder/abc204/D.cpp
@@ -0,0 +1,128 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define Th third
+#define nl cout << endl;
+#define pb push_back
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+using lld = long double;
+using ll = long long int;
+using ar = vector<ll>;
+using mat = vector<vector<ll>>;
+mat cn(ll n, ll m) {return vector<vector<ll>>(n, vector<ll>(m));}
+
+ll const MOD = 1e9 + 7;
+template<class T> inline bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
+template<class T> inline bool chmax(T &a, T b) { return a < b ? a = b, 1 : 0;}
+template<class T> inline void add(T &a, T b) { a += b; a >= MOD ? a - MOD : a; }
+template<class T> inline void sub(T &a, T b) { a -= b; a < 0 ? a + MOD : a; }
+template<class T> inline void mul(T &a, T b) {return (a * b) % MOD; }
+template<class T> void remDup(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), end(v)); }
+
+bool comp1(char &s1, char &s2) { return s1 > s2; }
+bool comp2(const pair<ll, pair<ll, ll>> &a, const pair<ll, pair<ll, ll>> &b) {
+      if (a.first > b.first)
+            return 1;
+      if (a.first == b.first && a.S.S > b.S.S)
+            return 1;
+      return 0;
+}
+class Pair { public: ll first, second;};
+bool comp3(const Pair &a, const Pair &b) {
+      if (a.F > b.F) return 1; return 0;
+}
+class Trips { public: ll first, second, third;};
+bool comp4(const Trips &a, const Trips &b) {
+      if (a.Th > b.Th) return 1; return 0;
+}
+
+void readArray(ll a[], int n) {
+      for (ll i = 0; i < n ; i++)
+            cin >> a[i];
+}
+
+ll const inf = 1e18;
+ll const mod = 1e9 + 7;
+ll const maxn = 2e3 + 1;
+
+vector<ll> adj[maxn];
+ll vis[maxn][maxn];
+void dfs(ll start, ll from, ll par = -1) {
+      vis[start][from] = 1;
+      for (ll to : adj[from]) {
+            if (!vis[start][to])
+                  dfs(start, to, from);
+      }
+}
+// void dfs(ll from, ll d, ll par = -1){
+
+//       ans += d
+
+
+// }
+void solve()
+{
+      ll t1 = 1;
+      // cin >> t;
+      for (ll tt = 1; tt <= t1; tt++) {
+
+            ll n; cin >> n;
+            ll a[n + 1];
+            f(i, 0, n) {cin >> a[i + 1];  }
+
+            ll sum = 0;
+            f(i, 1, n + 1) sum += a[i];
+            
+            ll dp[sum + 1][n + 1];
+            memset(dp, 0, sizeof dp);
+            for(ll i = 0; i <= n; i++){
+                  dp[0][i] = 1;
+            }
+            for (ll j = 1; j <= n; j++) {
+
+                  for (ll i = sum; i > 0; i--) {
+
+                        if (i - a[j] >= 0) {
+                              // cout << 1;
+                              dp[i][j] = dp[i - a[j]][j - 1] | dp[i][j - 1];
+                        }else dp[i][j] = dp[i][j - 1];
+                  }
+            }
+            ll ans = 0;
+            for (ll i = (sum + 1)/2 ; i <= sum; i++) {
+                  if(dp[i][n]){
+                        ans = i;
+                        break;
+                  }
+            }
+
+            // cout << sum << endl;
+            cout << ans << endl;
+      }
+}
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+      //*
+#ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+#endif/**/
+
+      solve();
+      // cout << "Case #" <<tt<<": ";
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
+      return 0;
+}
+
