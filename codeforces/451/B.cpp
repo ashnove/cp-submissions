5246f5b64bd56701182c973b743076026cd3ad9d
Ashutosh Kumar Shaw <aks.19u10584@btech.nitdgp.ac.in>
2021-06-12 22:56:00 +0530

    Add solution for problem `B - Sort the Array` Link: https://codeforces.com/contest/451/problem/B Tags: implementation, sortings, *1300 Ref: https://codeforces.com/contest/451/submission/119269608

---
+++ codeforces/451/B.cpp
@@ -0,0 +1,111 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define Th third
+#define nl cout << endl;
+#define pb push_back
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+using lld = long double;
+using ll = long long int;
+using ar = vector<ll>;
+using mat = vector<vector<ll>>;
+mat cn(ll n, ll m) {return vector<vector<ll>>(n, vector<ll>(m));}
+
+ll const MOD = 1000000007;
+template<class T> inline bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
+template<class T> inline bool chmax(T &a, T b) { return a < b ? a = b, 1 : 0;}
+template<class T> inline void add(T &a, T b) { a += b; a >= MOD ? a - MOD : a; }
+template<class T> inline void sub(T &a, T b) { a -= b; a < 0 ? a + MOD : a; }
+template<class T> inline void mul(T &a, T b) {return (a * b) % MOD; }
+template<class T> void remDup(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), end(v)); }
+
+bool comp1(char &s1, char &s2) { return s1 > s2; }
+bool comp2(const pair<ll, pair<ll, ll>> &a, const pair<ll, pair<ll, ll>> &b) {
+      if (a.first > b.first)
+            return 1;
+      if (a.first == b.first && a.S.S > b.S.S)
+            return 1;
+      return 0;
+}
+class Pair { public: ll first, second;};
+bool comp3(const Pair &a, const Pair &b) {
+      if (a.F > b.F) return 1; return 0;
+}
+class Trips { public: ll first, second, third;};
+bool comp4(const Trips &a, const Trips &b) {
+      if (a.Th > b.Th) return 1; return 0;
+}
+
+void readArray(ll a[], int n) {
+      for (ll i = 0; i < n ; i++)
+            cin >> a[i];
+}
+
+ll const inf = 1e18;
+// ll const mod = 1e9 + 7;
+ll const maxn = 1e5 + 1;
+
+ll modpow(ll a, ll b)
+{
+      ll res = 1;
+      while (b > 0) {
+            if ( b & 1 ) res = ( res * a ) % MOD;
+            a = ( a * a ) % MOD;
+            b /= 2;
+      }
+      return res % MOD;
+}
+void solve()
+{
+      ll t1 = 1;
+      // cin >> t1;
+      for (ll tt = 1; tt <= t1; tt++) {
+            ll n; cin >> n;
+            ll a[n]; f(i,0,n) cin >> a[i];
+            ll rinc[n]; rinc[n-1] = n-1;
+            ll ldec[n]; ldec[n-1] = n-1;
+            for(ll i = n - 2; i >= 0; i--){
+                  if(a[i] <= a[i + 1])rinc[i] = rinc[i + 1]; else rinc[i] = i;
+                  if(a[i] >= a[i + 1])ldec[i] = ldec[i + 1]; else ldec[i] = i;
+            }
+            vector<ll> ans;
+            ll x = rinc[0]; ans.pb(x);
+            x = ldec[x]; ans.pb(x);
+            x = rinc[x];
+            reverse(a + ans[0], a + ans[1] + 1);
+            bool ok = 1;
+            f(i,1,n) if(a[i] < a[i - 1]) ok = 0;
+            if(ok)
+                  cout << "yes\n" << ans[0]+1 << " " << ans[1]+1 << endl;
+            else cout << "no\n";
+      }
+}
+
+
+
+
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+      /*
+      #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+      #endif/**/
+
+      solve();
+      // cout << "Case #" <<tt<<": ";
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
+      return 0;
+}
