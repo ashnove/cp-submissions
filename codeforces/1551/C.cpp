8d2f4c67e41a4ea539182237224c937282326c86
Ashutosh Kumar Shaw <ashutoshkshaw@gmail.com>
2021-07-23 21:47:00 +0530

    Add solution for problem `C - Interesting Story` Link: https://codeforces.com/contest/1551/problem/C Tags: greedy, sortings, strings, *1500 Ref: https://codeforces.com/contest/1551/submission/123518141

---
+++ codeforces/1551/C.cpp
@@ -0,0 +1,125 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define Th third
+#define nl cout << endl;
+#define pb push_back
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+using lld = long double;
+using ll = long long int;
+using ar = vector<ll>;
+using mat = vector<vector<ll>>;
+mat cn(ll n, ll m) {return vector<vector<ll>>(n, vector<ll>(m));}
+
+ll const MOD = 1000000007;
+template<class T> inline bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
+template<class T> inline bool chmax(T &a, T b) { return a < b ? a = b, 1 : 0;}
+template<class T> inline void add(T &a, T b) { a += b; a >= MOD ? a - MOD : a; }
+template<class T> inline void sub(T &a, T b) { a -= b; a < 0 ? a + MOD : a; }
+template<class T> inline void mul(T &a, T b) {return (a * b) % MOD; }
+template<typename T> ostream& operator <<(ostream &out, const vector<T> &v) { for (auto &x : v) out << x << ' '; return out;}
+template<class T> void remDup(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), end(v)); }
+
+bool comp1(char &s1, char &s2) { return s1 > s2; }
+bool comp2(const  pair<ll, ll> &a, const pair<ll, ll> &b) {
+      if (a.first > b.first)
+            return 1;
+      if (a.first == b.first && a.S < b.S)
+            return 1;
+      return 0;
+}
+class Pair { public: ll first, second;};
+bool comp3(const Pair &a, const Pair &b) {
+      if (a.F > b.F) return 1; return 0;
+}
+class Trips { public: ll first, second, third;};
+bool comp4(const Trips &a, const Trips &b) {
+      if (a.Th > b.Th) return 1; return 0;
+}
+
+void readArray(ll a[], int n) {
+      for (ll i = 0; i < n ; i++)
+            cin >> a[i];
+}
+
+ll const inf = 1e18;
+// ll const mod = 1e9 + 7;
+int const maxn = 2e5 + 1;
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+void solve()
+{
+      ll t1 = 1;
+      cin >> t1;
+
+      for (ll tt = 1; tt <= t1; tt++) {
+            ll n; cin >> n;
+            string s[n];
+
+            for (ll i = 0; i < n; i++) cin >> s[i];
+            ll ans = 0;
+            for (char e = 'a'; e <= 'e'; e++) {
+
+                  vector<pair<ll, ll> > vv;
+                  vector<ll> onlyE;
+                  vector<ll> rest;
+                  ll tot = 0, e_cnt = 0;
+                  ll res = 0;
+                  for (ll i = 0; i < n; i++) {
+
+                        ll c = 0;
+                        for (ll j = 0; j < s[i].size(); j++)
+                              if (s[i][j] == e) c++;
+                        // tot += s[i].size() - c;
+                        // e_cnt += c;
+                        if(c == s[i].size()) e_cnt+=c, res++;
+                        else rest.pb(s[i].size() - 2 * c);
+                        // vv.pb({c , s[i].size() - c});
+                  }
+                  sort(all(rest));
+                  // reverse(all())
+                  ll pp  = 0;
+                  for (auto i : rest) {
+
+                        if (e_cnt - tot > 0) {
+                              pp = res;
+                        }
+                        tot += i;                       
+                        res++;
+
+                  }
+                  if (e_cnt - tot > 0) {
+                        pp = res;
+                  }
+                  // cout << e << " " << pp << endl;
+                  ans = max(pp, ans);
+
+            }
+            cout << ans << endl;
+      }
+}
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+      /*
+      #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+      #endif/**/
+
+      solve();
+      // cout << "Case #" <<tt<<": ";
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
+      return 0;
+}
