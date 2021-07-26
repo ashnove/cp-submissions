f00e4b236c85b9373d12136d25850294c4818dcc
Ashutosh Kumar Shaw <ashutoshkshaw@gmail.com>
2021-07-26 12:01:00 +0530

    Add solution for problem `B - Running for Gold` Link: https://codeforces.com/contest/1552/problem/B Tags: combinatorics, graphs, greedy, sortings, *1500 Ref: https://codeforces.com/contest/1552/submission/123803421

--- codeforces/1552/B.cpp
+++ codeforces/1552/B.cpp
@@ -1,7 +1,7 @@
 /*
 * @author : ashnove
 */
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>  
 using namespace std;
 
 #define pb push_back
@@ -83,7 +83,7 @@ void solve()
                   else  i++;
             }
             bool ok = 1;
-            for (int k = 0; k < n && ok; k++) {
+            for (int k = 0; k < n; k++) {
                   if (k != i) {
                         int c = 0;
                         for (int p = 0; p < 5; p++) {
