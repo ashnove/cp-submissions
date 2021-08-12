8376e5fdc3f3de4b21e337aff18d931fdbe1c02b
Ashutosh Kumar Shaw <ashutoshkshaw@gmail.com>
2021-08-12 20:19:00 +0530

    Add solution for problem `B - Soldier and Badges` Link: https://codeforces.com/contest/546/problem/B Tags: brute force, greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/546/submission/125680028

--- codeforces/546/B.cpp
+++ codeforces/546/B.cpp
@@ -1,33 +1,94 @@
+/*
+* @author : ashnove
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long int;
+#define pb push_back
+#define all(dp2) (dp2).begin(),(dp2).end()
+#define F first
+#define S second
+#define Th third
+#define nl cout << endl;
+#define pb push_back
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define what_is(dp2) cerr << #dp2 << " is " << dp2 << endl;
+
 using lld = long double;
+using ll = long long int;
+using ar = vector<ll>;
+using mat = vector<vector<ll>>;
+mat cn(ll n, ll m) {return vector<vector<ll>>(n, vector<ll>(m));}
 
-int main() {
+ll const MOD = 1000000007;
+template<class T> inline bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
+template<class T> inline bool chmadp2(T &a, T b) { return a < b ? a = b, 1 : 0;}
+template<class T> inline void add(T &a, T b) { a += b; a >= MOD ? a - MOD : a; }
+template<class T> inline void sub(T &a, T b) { a -= b; a < 0 ? a + MOD : a; }
+template<class T> inline void mul(T &a, T b) {return (a * b) % MOD; }
+template<typename T> ostream& operator <<(ostream &out, const vector<T> &v) { for (auto &dp2 : v) out << dp2 << ' '; return out;}
+template<class T> void remDup(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), end(v)); }
 
-ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool comp1(char &s1, char &s2) { return s1 > s2; }
+bool comp2(const pair<ll, pair<ll, ll>> &a, const pair<ll, pair<ll, ll>> &b) {
+      if (a.first > b.first)
+            return 1;
+      if (a.first == b.first && a.S.S > b.S.S)
+            return 1;
+      return 0;
+}
+class Pair { public: ll first, second;};
+bool comp3(const Pair &a, const Pair &b) {
+      if (a.F > b.F) return 1; return 0;
+}
+class Trips { public: ll first, second, third;};
+bool comp4(const Trips &a, const Trips &b) {
+      if (a.Th > b.Th) return 1; return 0;
+}
 
-
-      ll n; cin >> n;
-      vector<ll> a(n);
-      unordered_map<ll,ll> count;
-      for (ll i = 0; i < n; i++){
+void readArray(ll a[], int n) {
+      for (ll i = 0; i < n ; i++)
             cin >> a[i];
-            ++count[a[i]];
-      }
+}
 
-      ll cost = 0;
-      for (ll i = 1; i <= 2*n - 2; i++) {
+ll const inf = 1e9 + 7;
+int const maxn = 5e5 + 1;
 
-            if(count[i] > 1){
-                  ll shift = count[i] - 1;
-                  count[i + 1] += shift;
-                  cost += shift;
+
+void solve()
+{
+      ll t1 = 1;
+      // cin >> t1;
+      for (ll tt = 1; tt <= t1; tt++) {
+
+            ll n; cin >> n;
+            vector<ll> a(n);
+            for(ll i = 0; i < n; i++) cin >> a[i];
+
+            sort(all(a));
+
+            ll cost = 0;
+            for(ll i = 1; i < n; i++){
+
+                  if(a[i] <= a[i - 1]){
+                        cost += a[i - 1] - a[i] + 1;
+                        a[i] += a[i - 1] - a[i] + 1;
+                  }
+
             }
-
+            cout << cost;
       }
-      cout << cost;
 
+}
+
+int main()
+{
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+
+
+      solve();
+      // cout << "Case #" <<tt<<": ";
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
       return 0;
 }
