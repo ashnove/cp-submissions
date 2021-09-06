a905ed864227d667fe4c3ed3d0d769f4908039d6
Ashutosh Kumar Shaw <ashutoshkshaw@gmail.com>
2021-09-06 09:49:00 +0530

    Add solution for problem `C - The Tag Game` Link: https://codeforces.com/contest/813/problem/C Tags: dfs and similar, graphs, *1700 Ref: https://codeforces.com/contest/813/submission/128010650

---
+++ codeforces/813/C.cpp
@@ -0,0 +1,133 @@
+/*
+* @author : ashnove
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(dp2) (dp2).begin(),(dp2).end()
+#define F first
+#define S second
+#define Th third
+#define nl cout << endl;
+#define endl "\n";
+#define pb push_back
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define what_is(dp2) cerr << #dp2 << " is " << dp2 << endl;
+
+using lld = long double;
+using ll = long long int;
+using ar = vector<ll>;
+using mat = vector<vector<ll>>;
+mat cn(ll n, ll m) {return vector<vector<ll>>(n, vector<ll>(m));}
+
+ll const MOD = 1000000007;
+template<class T> inline bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
+template<class T> inline bool chmadp2(T &a, T b) { return a < b ? a = b, 1 : 0;}
+template<class T> inline void add(T &a, T b) { a += b; a >= MOD ? a - MOD : a; }
+template<class T> inline void sub(T &a, T b) { a -= b; a < 0 ? a + MOD : a; }
+template<class T> inline void mul(T &a, T b) {return (a * b) % MOD; }
+template<typename T> ostream& operator <<(ostream &out, const vector<T> &v) { for (auto &dp2 : v) out << dp2 << ' '; return out;}
+template<class T> void remDup(vector<T>& v) { sort(all(v)); v.erase(unique(all(v)), end(v)); }
+
+bool comp1(char &s1, char &s2) { return s1 > s2; }
+bool comp2(const pair<ll, pair<ll, ll>> &a, const pair<ll, pair<ll, ll>> &b) {
+      if (a.first > b.first)
+            return 1;
+      if (a.first == b.first && a.S.S > b.S.S)
+            return 1;
+      return 0;
+}
+class Pair { public: ll first, second;};
+bool comp3(const Pair &a, const Pair &b) {
+      if (a.F > b.F) return 1; return 0;
+}
+class Trips { public: ll first, second, third;};
+bool comp4(const Trips &a, const Trips &b) {
+      if (a.Th > b.Th) return 1; return 0;
+}
+
+void readArray(ll a[], int n) {
+      for (ll i = 0; i < n ; i++)
+            cin >> a[i];
+}
+
+ll const inf = 1e18;
+ll const maxn = 1e6 + 1;
+
+ll gcd(ll a, ll b) {
+      if (a == 0)
+            return b;
+      return gcd(b % a, a);
+}
+
+void solve() {
+
+      int T = 1;
+      // cin >> T;
+      for (ll tt = 1; tt <= T; tt++) {
+
+            int n,x; cin >> n >> x;
+            vector<int> adj[n + 1];
+            for(int i = 0; i < n - 1; i++){
+                  int u,v; cin >> u >> v;
+                  adj[u].pb(v);
+                  adj[v].pb(u);
+            }
+
+            queue<int> q;
+            q.push(1);
+            vector<int> vis(n + 1, 0), dis(n + 1, 0), par(n + 1, -1);
+            while(!q.empty()){
+                  int from = q.front(); q.pop();
+                  for(int to : adj[from]){
+                        if(!vis[to]){
+                              vis[to] = 1;
+                              par[to] = from;
+                              dis[to] = dis[from] + 1;
+                              q.push(to);
+                        }
+                  }
+            }
+            int times = (dis[x] - 1)/2;
+
+            int node = x;
+            while(times--){
+                  node = par[node];
+            }
+            fill(all(vis), 0);
+            vis[par[node]] = 1;
+            q.push(node);
+            int disMax = dis[node];
+            while(!q.empty()){
+                  int from = q.front(); q.pop();
+                  for(int to : adj[from]){
+                        if(!vis[to]){
+                              vis[to] = 1;
+                              disMax = max(disMax, dis[to]);
+                              dis[to] = dis[from] + 1;
+                              q.push(to);
+                        }
+                  }
+            }
+
+            cout << disMax*2 << endl;       
+      }
+
+}
+
+int main() {
+
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL); cout.tie(NULL);
+// #ifndef ONLINE_JUDGE
+//    freopen("consistency_chapter_1_input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+// #endif
+
+
+      solve();
+      // cout << "Case #" <<tt<<": ";
+      // printf("TIME: %.3lf\n", double(clock()) / CLOCKS_PER_SEC);
+      return 0;
+}
